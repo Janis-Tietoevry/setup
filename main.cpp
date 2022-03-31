d66a0cb5162d584b793c6d0c8c55ed22f4192485
Ran Nyman <ran@gosei.fi>
2022-03-31 15:33:53 +0300

    Renamed team files

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include "Team1Xx.h"
 #include "Xx.h"
 
 
 int main(int arc, char **argv) {
-    Xx x{};
+    //Uncomment your teams file to run
+    //Xx x{};
+    //Team1Xx x1{};
+    //std::cout << x1.getXx() << std::endl;
 }
