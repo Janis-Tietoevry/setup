80fdfdfe1334e99d2b1b85db9d89b918b3f58731
Janis Kalejs <Janis.Kalejs@tietoevry.com>
2022-04-22 12:35:51 +0300

    Team3: Add Subtraction and Multiple operators

--- Google_tests/team-3-tests/XXTest.cpp
+++ Google_tests/team-3-tests/XXTest.cpp
@@ -24,3 +24,21 @@ TEST_F (XXTest, TestSingleNumber){
 TEST_F (XXTest, TestTwoNumberMultiplication){
     ASSERT_EQ(15, x.calculate("5*3"));
 }
+
+TEST_F (XXTest, TestTwoNumberSubtraction){
+    ASSERT_EQ(5, x.calculate("7-2"));
+}
+
+TEST_F (XXTest, TestTwoOtherNumberSubtraction){
+    ASSERT_EQ(19, x.calculate("34-15"));
+}
+
+TEST_F (XXTest, TestThreeOperandSum){
+    ASSERT_EQ(25, x.calculate("3+7+15"));
+}
+
+TEST_F (XXTest, TestThreeOperandMix){
+    ASSERT_EQ(-23, x.calculate("7-10*3")); //wtf
+}
+
+
--- team-3-files/Team3Xx.cpp
+++ team-3-files/Team3Xx.cpp
@@ -5,21 +5,21 @@ Team3Xx::Team3Xx()
 int Team3Xx::calculate(const std::string& expr) const {
     if(expr.empty()) return 0;
 
-    auto op_pos = expr.find_first_of('+*');
+    auto op_pos = expr.find_first_of("*+-");
     if (op_pos == std::string::npos) {
         return std::stoi(expr);
     }
 
-    auto sub_left = expr.substr(0, op_pos);
-    auto sub_right = expr.substr(op_pos + 1);
+    auto left_nr = calculate(expr.substr(0, op_pos));
+    auto right_nr = calculate(expr.substr(op_pos + 1));
     auto op = expr.substr(op_pos, 1);
-
-    auto left_nr = std::stoi(sub_left);
-    auto right_nr = std::stoi(sub_right);
 
     if (op == "+") {
         return left_nr + right_nr;
     } else if (op == "*") {
         return left_nr * right_nr;
+    } else if (op == "-") {
+        return left_nr - right_nr;
     }
+
 }
